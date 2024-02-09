ab587648e64a77b28361a4efed8b23cde8bb6919
Rajesh10 <guddewadi10@gmail.com>
2024-02-09 18:42:42 +0530

    LinkedList

---
+++ src/ListInterface/Linkedlist/LinkedDemo.java
@@ -0,0 +1,29 @@
+package ListInterface.Linkedlist;
+
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.LinkedList;
+
+public class LinkedDemo {
+    public static void main(String[] args) {
+        LinkedList<String> fruits = new LinkedList<>();
+        fruits.add("Mango");
+        fruits.add("Grapes");
+        fruits.add("Apple");
+        fruits.add("Banana");
+
+        System.out.println(fruits);
+        Collections.sort(fruits);
+        System.out.println(fruits);
+        fruits.sort((s1, s2) -> s2.compareTo(s1));
+        System.out.println(fruits);
+
+        String str = fruits.get(2);
+        System.out.println(str);
+
+        Iterator<String> itr = fruits.iterator();
+        while (itr.hasNext()){
+            System.out.println(itr.next());
+        }
+    }
+}
--- src/ListInterface/ListLinkedList.java
+++ src/ListInterface/ListLinkedList.java
@@ -1,3 +1,5 @@
+package ListInterface;
+
 import java.util.LinkedList;
 
 public class ListLinkedList {
